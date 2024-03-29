a8b5739bee432cf5e4c5531fd6c03d14c58fbc16
rim109 <fla7722@naver.com>
2024-03-29 12:22:39 +0900

    ⌛Refactor: CORS 부분 수정

--- src/main/kotlin/com/example/days/global/infra/web/WebConfig.kt
+++ src/main/kotlin/com/example/days/global/infra/web/WebConfig.kt
@@ -1,12 +1,15 @@
 package com.example.days.global.infra.web
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    @Value("\${domain.url}") private val domainUrl: List<String>
+) : WebMvcConfigurer {
+
 
 //    override fun addFormatters(registry: FormatterRegistry) {
 //        registry.addConverter(OAuth2ProviderConverter())
@@ -14,8 +17,10 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해서
-            .allowedOrigins("http://localhost:8080","https://api.100days.life","http://localhost:8090") // 이 출처로부터의 요청만 허용
-            .allowedMethods("GET","POST","PUT","DELETE","PATCH","HEAD","OPTIONS")
+            .allowedOrigins(
+                *domainUrl.toTypedArray()
+            ) // 이 출처로부터의 요청만 허용
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true) // 쿠키를 포함한 요청 허용
             .maxAge(3000)
